7fd3c6e40bd50c4e0220d15af4e5eb90821c1569
rmodrak <rmodrak@gmail.com>
2021-08-03 18:46:29 -0800

    Updated download waveforms script

--- _download_waveforms.bash
+++ _download_waveforms.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # navigate to mtbench/setup
 cd $(dirname ${BASH_SOURCE[0]})
@@ -6,7 +6,11 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 URL="https://github.com/rmodrak/mtbench.git"
 
-mkdir ../input/
-cd ../input/
-git clone --branch waveforms $URL
+git clone --branch input $URL ./input
+
+cd ./input
+cat Alvizuri2018.tgz.part* > Alvizuri2018.tgz
+cat Silwal2016.tgz.part* > Silwal2016.tgz
+tar -xzf Alvizuri2018.tgz
+tar -xzf Silwal2016.tgz
 
