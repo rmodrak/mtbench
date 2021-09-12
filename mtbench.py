52a6c76bbf0fecf22181b5929b97ecf5e05e592e
Ryan Modrak <rmodrak@gmail.com>
2021-09-12 00:19:13 -0600

    Import stats utilities from mtuq

--- mtbench.py
+++ mtbench.py
@@ -8,6 +8,7 @@ from mtuq import read, open_db, download_greens_tensors
 from mtuq.graphics import plot_data_greens1, plot_data_greens2, plot_misfit_lune
 from mtuq.grid_search import DataArray, DataFrame, grid_search
 from mtuq.misfit import Misfit
+from mtuq.misfit.waveform._stats import estimate_sigma, calculate_norm_data
 from mtuq.util.cap import parse_station_codes, Trapezoid
 from mtuq.util.math import list_intersect_with_indices
 from mtuq.util.signal import get_components
@@ -82,9 +83,11 @@ def bench(
 
     data.sort_by_distance()
     stations = data.get_stations()
+    for station in stations: print(station, '\n') # DEBUG
 
     origin = data.get_origins()[0]
     origin.depth_in_m = depth
+    print(origin)
 
     processed_data = []
     for process_data in data_processing:
@@ -93,6 +96,7 @@ def bench(
 
     print('Reading Green''s functions...\n')
 
+    print('SOLVER:', solver)
     db = open_db(path_greens, format=solver,
         model=model, include_mt=include_mt, include_force=include_force)
 
@@ -140,7 +144,7 @@ def bench(
             for component in groups[0]:
                components += [component]
 
-            sigma = calculate_sigma(processed_data[_i], processed_greens[_i],
+            sigma = estimate_sigma(processed_data[_i], processed_greens[_i],
                 best_source, misfit.norm, components,
                 misfit.time_shift_min, misfit.time_shift_max)
 
@@ -197,96 +201,6 @@ def bench(
 
     print('\nFinished\n')
 
-
-
-def calculate_norm_data(data, norm, components):
-    # error checking
-    assert norm in ('L1', 'L2')
-
-    norm_data = 0.
-    for _j, d in enumerate(data):
-        _components, indices = list_intersect_with_indices(
-            get_components(d), components)
-
-        if not indices:
-            continue
-
-        # time sampling scheme
-        npts = d[0].data.size
-        dt = d[0].stats.delta
-
-        for _k in indices:
-            r = d[_k].data
-
-            if norm=='L1':
-                norm_data += np.sum(np.abs(r))*dt
-
-            elif norm=='L2':
-                norm_data += np.sum(r**2)*dt
-
-    return norm_data
-
-
-
-def calculate_sigma(data, greens, best_source, norm, components,
-    time_shift_min, time_shift_max):
-
-    # error checking
-    assert norm in ('L1', 'L2')
-
-    residuals = []
-    for _j, d in enumerate(data):
-        _components, indices = list_intersect_with_indices(
-            components, get_components(d))
-
-        if not indices:
-            continue
-
-        # generate synthetics
-        greens[_j]._set_components(get_components(d))
-        s = greens[_j].get_synthetics(best_source)
-
-        # time sampling scheme
-        npts = d[0].data.size
-        dt = d[0].stats.delta
-
-        padding_left = int(+time_shift_max/dt)
-        padding_right = int(-time_shift_min/dt)
-        npts_padding = padding_left + padding_right
-
-        # array to hold cross correlations
-        corr = np.zeros(npts_padding+1)
-
-        #
-        # calculate residuals
-        # 
-
-        corr[:] = 0.
-        for _k in indices:
-            corr += np.correlate(s[_k].data, d[_k].data, 'valid')
-
-        npts_shift = padding_left - corr.argmax()
-        time_shift = npts_shift*dt
-
-        # what start and stop indices will correctly shift synthetics
-        # relative to data?
-        start = padding_left - npts_shift
-        stop = start + npts
-
-        for _k in indices:
-
-            # substract data from shifted synthetics
-            r = s[_k].data[start:stop] - d[_k].data
-
-            # sum the resulting residuals
-            if norm=='L1':
-                residuals += [np.sum(np.abs(r))*dt]
-
-            elif norm=='L2':
-                residuals += [np.sum(r**2)*dt]
-
-
-    return np.mean(residuals)**0.5
 
 
 def _plot_waveforms(filename,
